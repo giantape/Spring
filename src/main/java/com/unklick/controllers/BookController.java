1a3b1cc06ccfb3703b8afbcaeea43f6d0a110db3
sapinodejs@gmail.com <>
2019-11-23 22:42:52 +0100

    update all repo and adding view

--- src/main/java/com/unklick/controllers/BookController.java
+++ src/main/java/com/unklick/controllers/BookController.java
@@ -7,16 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BookController {
+
     private BookRepository bookRepository;
-    public void BookController(BookRepository bookRepository){
+
+    public BookController(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
+
     @RequestMapping("/books")
-    public String getBook(Model model){
-        model.addAttribute("book", bookRepository.findAll());
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
         return "books";
     }
-
-
-
 }
